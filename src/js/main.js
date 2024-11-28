a6148fb457aaa55bf320c0f521ca86836b46ee39
seren <seren.coban@student.hepl.be>
2024-11-28 18:46:03 +0100

    finished commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,22 @@
+
 (function (){
-    const passwordApp = {
-         passwordInput : document.getElementById('password'),
-         buttonElt : document.querySelector('.showPass'),
-        init(){
-            document.documentElement.classList.add('js-enabled')
+    const passwordApp ={
+        passwordInput: document.getElementById('password'),
+        buttonElt: document.querySelector('.showPass'),
+        init() {
+            document.documentElement.classList.add('js-enabled');
             this.passwordTest();
         },
         passwordTest(){
-            this.buttonElt.addEventListener('click', () => {
-                if (this.passwordInput.type === 'password') {
+            this.buttonElt.addEventListener('click', ()=> {
+                if(this.passwordInput.type === 'password'){
                     this.passwordInput.type = 'text';
-                } else {
-                    this.passwordInput.type = 'password';
-                    this.buttonElt.textContent = 'Montrer';
+                }else{
+                    this.passwordInput.type ='password';
                 }
             });
         }
+
     }
     passwordApp.init();
 })();
